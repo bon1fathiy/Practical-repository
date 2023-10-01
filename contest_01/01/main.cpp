86313ce37115364f0cb12a83786fd053e2a4a87a
bon1fathiy <144465692+bon1fathiy@users.noreply.github.com>
2023-10-01 19:26:47 +0300

    Update main.cpp

--- contest_01/01/main.cpp
+++ contest_01/01/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main()
+{
+	float a = sqrt(12);
+	float b = 1/(3*3.0);
+	float c = 1/(5*3*3.0);
+	float d = 1/(7*3*3*3.0);
+	float e = 1/(9*3*3*3*3.0);
+	float f = 1/(11*3*3*3*3*3.0);
+
+	float g = a*(1-b+c-d+e-f);
+
+	cout << g;
