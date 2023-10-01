0ab690ef6a0aaeea261e976aea70f0551fe37d7a
bon1fathiy <144465692+bon1fathiy@users.noreply.github.com>
2023-10-01 20:25:24 +0300

    Update main.cpp

--- contest_02/01/main.cpp
+++ contest_02/01/main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <cmath>
 
+int is_prime(int number) {
+    if (number < 2) {
+        return false;
+    }
+
+    for (int i = 2; i <= std::sqrt(number); i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
